53aa0496034c7b454a828d3d03be586ad0db73ce
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-10-08 08:22:35 +0000

    update youtubeVideo and mp3 update path location termux

--- installTermux.sh
+++ installTermux.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-pkg upgrade -y && pkg install nano curl git python python-pip aria2 wget ffmpeg -y && pip install yt-dlp -y
+pkg upgrade -y && pkg install nano curl git python python-pip aria2 wget ffmpeg -y && pip install yt-dlp
 
-mkdir .shortcuts && mv dotfiles/youtubemp3/youtubemp3Termux.sh .shortcuts && mv dotfiles/youtubeVideo/youtubeVideoTermux.sh .shortcuts && mv dotfiles/.aria2 ${HOME}
+mkdir .shortcuts && mv dotfiles/youtubemp3/youtubemp3Termux.sh .shortcuts && mv dotfiles/youtubeVideo/youtubeVideoTermux.sh .shortcuts && mv dotfiles/.aria2 ${HOME} && mkdir storage/downloads/youtubeVideo/ && mkdir storage/downloads/youtubeMp3/
+
 
 chmod +x .shortcuts/youtubemp3Termux.sh
 chmod +x .shortcuts/youtubeVideoTermux.sh
