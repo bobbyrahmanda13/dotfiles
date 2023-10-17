c0d52e6823034856dd3c5e98b114861674c634f5
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-10-17 13:10:56 +0000

    add aria2Termux.sh to termux app

--- installTermux.sh
+++ installTermux.sh
@@ -2,7 +2,7 @@
 pkg upgrade -y && pkg install nano curl git python python-pip aria2 wget ffmpeg -y && pip install yt-dlp
 
 mkdir .shortcuts
-mv dotfiles/youtubemp3/youtubemp3Termux.sh ${HOME}/.shortcuts && mv dotfiles/youtubeVideo/youtubeVideoTermux.sh ${HOME}/.shortcuts && mv dotfiles/.aria2 ${HOME}
+mv dotfiles/youtubemp3/youtubemp3Termux.sh ${HOME}/.shortcuts && mv dotfiles/youtubeVideo/youtubeVideoTermux.sh ${HOME}/.shortcuts && mv dotfiles/.aria2/aria2Termux.sh ${HOME}/.shortcuts && mv dotfiles/.aria2 ${HOME}
 
 termux-setup-storage
 mkdir storage/downloads/youtubeVideo && mkdir storage/downloads/youtubeMp3
@@ -10,6 +10,7 @@ mkdir storage/downloads/youtubeVideo && mkdir storage/downloads/youtubeMp3
 
 chmod +x .shortcuts/youtubemp3Termux.sh
 chmod +x .shortcuts/youtubeVideoTermux.sh
+chmod +x .shortcuts/aria2Termux.sh
 
 rm -irf dotfiles/alacritty
 rm -irf dotfiles/iosevka_rahman
