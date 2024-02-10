9c0338270b9cf1e87fd9a627829bf988bbcc517b
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-02-10 19:17:13 +0700

    update

---
+++ mountPartition.sh
@@ -0,0 +1,4 @@
+cd /
+mkdir /media/rahman/Black && mkdir /media/rahman/Blue
+sudo mount /dev/sda2 /media/rahman/Black && sudo mount /dev/sda3 /media/rahman/Blue
+
--- moveDotfile.sh
+++ moveDotfile.sh
@@ -1,9 +1,10 @@
-path="/media/rahman/Black/rahmanapps/dotfiles"
+path="/media/rahman/Black/rahmanapps/dotfiles/"
 cp .zshrc $path
 cp .gitconfig $path
 cp .tmux.conf $path
 cp moveDotfile.sh $path
 cp gsend.sh $path
+cp mountPartition.sh $path
 cp -r ${HOME}/.config/alacritty $path
 cp -r ${HOME}/.config/gtk-3.0 $path
 cp -r ${HOME}/.aria2 $path
