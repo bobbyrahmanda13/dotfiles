ab73cb52d7b6db6129f95a3c0dec2da6b5d1b9ae
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-18 09:15:12 -0400

    chore: add kitty

--- moveDotfile.sh
+++ moveDotfile.sh
@@ -5,8 +5,9 @@ cp .tmux.conf $path
 cp moveDotfile.sh $path
 cp gsend.sh $path
 cp mountPartition.sh $path
-cp -r ${HOME}/.config/alacritty $path
+# cp -r ${HOME}/.config/alacritty $path
 cp -r ${HOME}/.config/gtk-3.0 $path
 cp -r ${HOME}/.aria2 $path
+cp -r ${HOME}/.config/kitty $path
 cd $path
 bash ${HOME}/gsend.sh
