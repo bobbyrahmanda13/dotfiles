79f4a0fa36382cfb61a6cc276474998b9372cf00
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-24 07:40:03 +0700

    chore: update

--- .aria2/aria2.sh
+++ .aria2/aria2.sh
@@ -1,6 +1,6 @@
 mousepad ${HOME}/.aria2/link.txt
 notify-send "Download Started"
-aria2c -i ${HOME}/.aria2/link.txt > .aria2/aria2_log.txt 2>&1
-# aria2c -i ${HOME}/.aria2/link.txt
+# aria2c -i ${HOME}/.aria2/link.txt > .aria2/aria2_log.txt 2>&1
+aria2c -i ${HOME}/.aria2/link.txt
 notify-send "Download Completed"
 exit
