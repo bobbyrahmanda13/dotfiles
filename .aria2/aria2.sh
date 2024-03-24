5e5ec9599fa5cb1123adbb5f4e29b186f45b2ad6
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-24 10:43:54 +0700

    update

--- .aria2/aria2.sh
+++ .aria2/aria2.sh
@@ -1,6 +1,14 @@
-mousepad ${HOME}/.aria2/link.txt
-notify-send "Download Started"
-# aria2c -i ${HOME}/.aria2/link.txt > .aria2/aria2_log.txt 2>&1
-aria2c -i ${HOME}/.aria2/link.txt
-notify-send "Download Completed"
-exit
+file="${HOME}/.aria2/link.txt"
+
+mousepad $file
+
+if ! grep -q '[^[:space:]]' $file; then
+  echo "file is empty" 
+else 
+  notify-send "Download Started"
+  # aria2c -i ${HOME}/.aria2/link.txt > .aria2/aria2_log.txt 2>&1
+  aria2c -i $file
+  notify-send "Download Completed"
+  truncate -s 0 $file
+  exit
+fi
