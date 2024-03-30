220ed1310148deac91e6e1ac7d52730722a312e4
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-30 14:58:11 +0700

    update alacritty

--- moveDotfile.sh
+++ moveDotfile.sh
@@ -5,7 +5,7 @@ cp .tmux.conf $path
 cp moveDotfile.sh $path
 cp gsend.sh $path
 # cp mountPartition.sh $path
-# cp -r ${HOME}/.config/alacritty $path
+cp -r ${HOME}/.config/alacritty $path
 cp -r ${HOME}/.config/gtk-3.0 $path
 cp -r ${HOME}/.aria2 $path
 # cp -r ${HOME}/.config/kitty $path
