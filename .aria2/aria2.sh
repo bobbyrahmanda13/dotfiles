d4c88205c7fa0452a99ab4c1635ecef2a72b5509
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-04-15 18:39:04 +0700

    fix: change to old code

--- .aria2/aria2.sh
+++ .aria2/aria2.sh
@@ -1,11 +1,4 @@
 file="${HOME}/.aria2/link.txt"
-path_download="${HOME}/Downloads"
-
-read -p "Location Downloads : " pathDownloads 
-
-if [ -z "$pathDownloads" ]; then
-  pathDownloads="$path_download"
-fi
 
 nvim $file
 
