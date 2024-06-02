f7ab8acdd749ab207f3edb94ea7b6aee7c55f59d
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-06-02 16:27:33 +0700

    update

--- mvdotfilesarch.sh
+++ mvdotfilesarch.sh
@@ -1,12 +1,11 @@
 path="${HOME}/Rahman/github/dotfiles"
-cp .zshrc $path
-cp .bashrc $path
-cp .gitconfig $path
-cp .tmux.conf $path
-cp mvdotfilesarch.sh $path
-cp repo-github.sh $path
-cp gsend.sh $path
-# cp mountPartition.sh $path
+cp ${HOME}/.zshrc $path
+cp ${HOME}/.bashrc $path
+cp ${HOME}/.gitconfig $path
+cp ${HOME}/.tmux.conf $path
+cp ${HOME}/mvdotfilesarch.sh $path
+cp ${HOME}/repo-github.sh $path
+cp ${HOME}/gsend.sh $path
 cp -rf ${HOME}/.config/alacritty $path
 cp -rf ${HOME}/.config/starship $path
 cp -rf ${HOME}/.config/gtk-3.0 $path
