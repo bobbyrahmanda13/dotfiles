733b2c360fa8f7f354b895c1631c03b7f15b559e
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-06-02 13:51:23 +0700

    update

--- archpackage.sh
+++ archpackage.sh
@@ -1,9 +1,8 @@
 sudo pacman -Syyu
-sudo pacman -Syy git telegram-dekstop tldr cmake unzip eza zoxide aria2 yt-dlp alacritty curl wget strawberry go libreoffice-still keepassxc ripgrep fd
+sudo pacman -Syy git telegram-dekstop tldr cmake vlc zip unzip eza zoxide fzf aria2 yt-dlp alacritty curl strawberry go libreoffice-fresh keepassxc ripgrep fd
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd
-yay -S brave-nightly-bin
-git clone https://github.com/neovim/neovim && cd neovim && make distclean && make CMAKE_BUILD_TYPE=RelWithDebInfo && sudo make install && cd .. && sudo rm -irf ${HOME}/neovim
+yay -S brave-bin
--- mountPartition.sh
+++
@@ -1,4 +0,0 @@
-cd /
-mkdir /media/rahman/Black && mkdir /media/rahman/Blue
-sudo mount /dev/sda2 /media/rahman/Black && sudo mount /dev/sda3 /media/rahman/Blue
-
