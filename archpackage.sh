f403fd030ca988e673263945839f29fd135691dc
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-06-06 19:00:31 +0700

    update

--- archpackage.sh
+++ archpackage.sh
@@ -1,5 +1,4 @@
-sudo pacman -Syyu
-sudo pacman -Syy git telegram-dekstop tldr cmake vlc zip unzip eza zoxide fzf aria2 yt-dlp alacritty curl strawberry go libreoffice-fresh keepassxc ripgrep fd
+sudo pacman -Syy git telegram-dekstop tldr cmake vlc zip unzip eza zoxide fzf aria2 yt-dlp alacritty curl strawberry go libreoffice-fresh keepassxc ripgrep fd odt2txt mediainfo calibre
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
