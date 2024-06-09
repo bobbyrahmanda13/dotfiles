8e5ccd86a06cd14653bef7f295f5355a0f4a2066
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-06-09 13:02:30 +0700

    update archpackage

--- archpackage.sh
+++ archpackage.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syy git telegram-dekstop tldr cmake vlc zip unzip eza zoxide fzf aria2 yt-dlp alacritty curl strawberry go libreoffice-fresh keepassxc ripgrep fd odt2txt mediainfo neovim ttf-fantasque-nerd mtpfs gvfs-mtp opensmtpd
+sudo pacman -Syy git telegram-dekstop tldr cmake vlc zip unzip eza zoxide fzf aria2 yt-dlp alacritty curl strawberry go libreoffice-fresh keepassxc ripgrep fd odt2txt mediainfo neovim ttf-fantasque-nerd mtpfs gvfs-mtp gvfs-gphoto2 opensmtpd
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
