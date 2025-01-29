23987bea4f13ba486ee563835a63d12973c84b51
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-01-29 21:19:50 +0700

    update

--- moveArchHome.sh
+++ moveArchHome.sh
@@ -1,11 +1,11 @@
 pathArch="${HOME}"
 patArchConfig="${HOME}/.config/"
-cp -rf .aria2 $pathArch
-cp -rf alacritty $pathArchConfig
-cp -rf gtk-3.0 $pathArchConfig
-cp -rf starship $pathArchConfig
-cp -rf youtubemp3 $pathArch
-cp -rf youtubeVideo $pathArch
+cp -r .aria2 $pathArch
+cp -r alacritty $pathArchConfig
+cp -r gtk-3.0 $pathArchConfig
+cp -r starship $pathArchConfig
+cp -r youtubemp3 $pathArch
+cp -r youtubeVideo $pathArch
 cp .gitconfig $pathArch
 cp .tmux.conf $pathArch
 cp .bashrc $pathArch
